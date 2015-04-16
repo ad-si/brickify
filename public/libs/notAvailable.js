a2105c1efd13388aaad49cf39efe6b57b3e9c7de
Yannis Kommana <yannis.kommana@student.hpi.de>
2015-04-16 15:55:16 +0200

    rename to notAvailable

--- public/libs/notAvailable.js
+++ public/libs/notAvailable.js
@@ -3,11 +3,11 @@
 	var alertCallback = function() {
 		return bootbox.alert({
 			title: 'Download not available',
-			message: 'We are sorry, but this is not yet available.' +
+			message: 'We are sorry, but this download is not yet available.' +
 			' Please check back later</br></br>' +
 			' Click the image on the left to load and edit the model.'
 		});
 	};
 
-	$('.notImplemented').click(alertCallback);
+	$('.notAvailable').click(alertCallback);
 }();
